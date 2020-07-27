910fdbf2f700f74bb3effec270507eada3079958
Fernando <fernandoalcioneidesouza@gmail.com>
2020-07-27 08:29:29 -0300

    Adicionando build do projeto

---
+++ build/precache-manifest.882cd005626029b7da801e3db360e5f3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9d8d86fa2635bc4f539b8e13fdbd5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63053118a3294e1be548",
+    "url": "/static/css/3.098bdb9f.chunk.css"
+  },
+  {
+    "revision": "974f542f8b276b00d2a9",
+    "url": "/static/css/4.80ebf5ae.chunk.css"
+  },
+  {
+    "revision": "cef3a360769ae7b85318",
+    "url": "/static/css/5.199e990c.chunk.css"
+  },
+  {
+    "revision": "af2ab3e9dfe6988c196b",
+    "url": "/static/css/main.11310d68.chunk.css"
+  },
+  {
+    "revision": "1fe318188be6d45614a7",
+    "url": "/static/js/2.a3a324fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a3a324fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63053118a3294e1be548",
+    "url": "/static/js/3.76e8f6b1.chunk.js"
+  },
+  {
+    "revision": "974f542f8b276b00d2a9",
+    "url": "/static/js/4.65bb380c.chunk.js"
+  },
+  {
+    "revision": "cef3a360769ae7b85318",
+    "url": "/static/js/5.7f424e3b.chunk.js"
+  },
+  {
+    "revision": "af2ab3e9dfe6988c196b",
+    "url": "/static/js/main.858239ee.chunk.js"
+  },
+  {
+    "revision": "a9a5a11ac4fa8e8d2f14",
+    "url": "/static/js/runtime-main.d6c0679a.js"
+  },
+  {
+    "revision": "952484d20ee26171d51bbcd4723757b4",
+    "url": "/static/media/chevron-left.952484d2.svg"
+  },
+  {
+    "revision": "67ede0f529ef3035123876f04a682657",
+    "url": "/static/media/chevron-right.67ede0f5.svg"
+  },
+  {
+    "revision": "506c846118ee3fcaf87900ba0b90f00f",
+    "url": "/static/media/close.506c8461.svg"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/static/media/loading.be00fc4a.svg"
+  },
+  {
+    "revision": "91d7f6149a8abe97a444b28300a94400",
+    "url": "/static/media/logo.91d7f614.svg"
+  },
+  {
+    "revision": "ecef733a33ceec8dc32d29510998afe9",
+    "url": "/static/media/search.ecef733a.svg"
+  }
+]);
